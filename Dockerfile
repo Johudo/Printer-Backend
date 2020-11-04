5f3702b2903b304309801c9f0cdebc6a702abc07
root <root@89-108-99-51.cloudvps.regruhosting.ru>
2020-11-04 16:28:02 +0300

    Deployment fixes

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ ENV STATIC_URL /static
 ENV STATIC_PATH /var/www/Printer-Backend/app/static
 COPY ./requirements.txt /var/www/Printer-Backend/requirements.txt
 RUN pip install -r /var/www/Printer-Backend/requirements.txt
+RUN mkdir /var/www/downloaded_files_printer
--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 
-# DEVELOPMENT
-UPLOAD_FOLDER = r"c:\Users\dimaz\Desktop\Printer Project\printer_uploaded_files"
-# # PRODUCTION
-# UPLOAD_FOLDER = "/var/www/downloaded_files_printer"
+# # DEVELOPMENT
+# UPLOAD_FOLDER = r"c:\Users\dimaz\Desktop\Printer Project\printer_uploaded_files"
+# PRODUCTION
+UPLOAD_FOLDER = "/var/www/downloaded_files_printer"
 
 app = Flask(__name__)
 CORS(app)
