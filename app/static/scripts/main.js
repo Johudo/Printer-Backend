e4a597b5f67d3d1962a63e33285aa91ae5fc93e2
Johudo <dimazuev11@gmail.com>
2021-03-20 17:41:25 +0300

    fixed axios

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,3 +1,5 @@
+// import axios from "axios";
+
 // var SERVER_HOST = 'http://127.0.0.1:5000/api';
 // var SERVER_HOST = 'http://dm-zuev.ru:56733/api'
 
---
+++ server.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install python3-pip python3-venv
+
+# install libs 
+pip3 install flask
+pip3 install flask_api
+pip3 install flask_cors
+
